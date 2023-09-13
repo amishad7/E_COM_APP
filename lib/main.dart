75dbd70e2f39858814b47b3a9e5fb9b37c24df10
r32 <r38@R38s-Mac-mini.local>
2023-09-13 15:10:06 +0530

    second upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,24 @@
 import 'package:e_com_app/utils/Global.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 void main() {
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: e_comerce_app(),
     ),
   );
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class e_comerce_app extends StatefulWidget {
+  const e_comerce_app({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<e_comerce_app> createState() => _e_comerce_appState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _e_comerce_appState extends State<e_comerce_app> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -77,7 +78,7 @@ class _HomePageState extends State<HomePage> {
                     return Container(
                       padding: const EdgeInsets.all(10),
                       width: 400,
-                      height: 310,
+                      height: 370,
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.start,
                         crossAxisAlignment: CrossAxisAlignment.start,
@@ -97,66 +98,113 @@ class _HomePageState extends State<HomePage> {
                                 ...data.map(
                                   (e) {
                                     return Container(
+                                      width: 190,
+                                      height: 290,
+                                      margin: const EdgeInsets.all(10),
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(25),
+                                        color: Colors.white,
+                                        boxShadow: const [
+                                          BoxShadow(
+                                            color: Colors.grey,
+                                            blurRadius: 0.1,
+                                            spreadRadius: 0.1,
+                                            offset: Offset(1, 1),
+                                          ),
+                                        ],
+                                      ),
                                       child: Column(
                                         crossAxisAlignment:
                                             CrossAxisAlignment.start,
                                         children: [
                                           Expanded(
+                                            flex: 2,
                                             child: Container(
                                               decoration: BoxDecoration(
-                                                borderRadius: BorderRadius.only(
-                                                  topLeft: Radius.circular(10),
-                                                  topRight: Radius.circular(10),
+                                                borderRadius:
+                                                    const BorderRadius.only(
+                                                  topLeft: Radius.circular(20),
+                                                  topRight: Radius.circular(20),
                                                 ),
-                                                color: Colors.deepOrange,
+                                                image: DecorationImage(
+                                                  image: AssetImage(
+                                                      "${e['images']}"),
+                                                  fit: BoxFit.cover,
+                                                ),
+                                              ),
+                                              alignment: Alignment.topLeft,
+                                              child: Container(
+                                                width: 65,
+                                                height: 34,
+                                                child: Center(
+                                                  child: Text(
+                                                    '${e['discountPercentage']}%',
+                                                    style: TextStyle(
+                                                      color: Colors.white,
+                                                      fontWeight:
+                                                          FontWeight.w800,
+                                                    ),
+                                                  ),
+                                                ),
+                                                decoration: BoxDecoration(
+                                                    color: Colors.red.shade800,
+                                                    borderRadius:
+                                                        BorderRadius.only(
+                                                      topLeft:
+                                                          Radius.circular(10),
+                                                      bottomRight:
+                                                          Radius.circular(07),
+                                                    )),
                                               ),
                                             ),
-                                            flex: 2,
                                           ),
                                           Expanded(
+                                            flex: 1,
                                             child: Container(
+                                              padding: const EdgeInsets.all(10),
                                               child: Column(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment
+                                                        .spaceBetween,
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
                                                 children: [
                                                   Text(
                                                     '${e['title']}',
-                                                    style: TextStyle(
+                                                    style: const TextStyle(
                                                       fontWeight:
                                                           FontWeight.w700,
                                                       fontSize: 15,
                                                     ),
                                                   ),
                                                   Text(
-                                                    '${e['price']}',
-                                                    style: TextStyle(
+                                                    '\$ ${e['price']}',
+                                                    style: const TextStyle(
                                                       fontWeight:
                                                           FontWeight.bold,
-                                                      fontSize: 20,
+                                                      fontSize: 18,
                                                     ),
                                                   ),
+                                                  RatingBar.builder(
+                                                    initialRating: 3,
+                                                    minRating: 1,
+                                                    direction: Axis.horizontal,
+                                                    allowHalfRating: true,
+                                                    itemCount: 5,
+                                                    ignoreGestures: true,
+                                                    itemSize: 18,
+                                                    itemBuilder: (context, _) =>
+                                                        const Icon(
+                                                      Icons.star,
+                                                      color: Colors.amber,
+                                                    ),
+                                                    onRatingUpdate: (rating) {
+                                                      print(rating);
+                                                    },
+                                                  ),
                                                 ],
-                                                mainAxisAlignment:
-                                                    MainAxisAlignment
-                                                        .spaceBetween,
-                                                crossAxisAlignment:
-                                                    CrossAxisAlignment.start,
                                               ),
-                                              padding: EdgeInsets.all(10),
                                             ),
-                                            flex: 1,
-                                          ),
-                                        ],
-                                      ),
-                                      width: 150,
-                                      height: 240,
-                                      margin: const EdgeInsets.all(10),
-                                      decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(20),
-                                        color: Colors.white,
-                                        boxShadow: const [
-                                          BoxShadow(
-                                            color: Colors.grey,
-                                            blurRadius: 0.1,
-                                            offset: Offset(2, 1),
                                           ),
                                         ],
                                       ),
--- lib/utils/Global.dart
+++ lib/utils/Global.dart
@@ -1,40 +1,98 @@
 List allProducts = [
   {
-    'CatogeryName': 'smartphones',
+    'CatogeryName': 'Headphones',
     'data': [
       {
         "id": 1,
-        "title": "iPhone 9",
+        "title": "Sony MDR-ZX110AP",
         "price": 549,
         "discountPercentage": 12.96,
         "rating": 4.69,
+        "images": "assets/Images/images (1).jpeg",
+      },
+      {
+        "id": 2,
+        "title": "Sony WH-XB900N ",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "images":
+            '/Users/r38/StudioProjects/e_com_app/assets/Images/gsmarena_001.jpg',
+      },
+      {
+        "id": 3,
+        "title": "JBL E45B",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "images":
+            '/Users/r38/StudioProjects/e_com_app/assets/Images/images.jpeg',
+      },
+      {
+        "id": 4,
+        "title": "Apple AirPods Pro",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "images": 'assets/Images/download (1).jpeg',
+      },
+      {
+        "id": 5,
+        "title": "Bose QuietComfort 35 II",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "images":
+            "/Users/r38/StudioProjects/e_com_app/assets/Images/download.jpeg",
       },
     ],
   },
   {
-    'CatogeryName': 'Laptop',
+    'CatogeryName': 'Laptops',
     'data': [
       {
         "id": 1,
-        "title": "Mac mini",
-        "price": 50000,
+        "title": "Sony MDR-ZX110AP",
+        "price": 549,
         "discountPercentage": 12.96,
-        "rating": 5,
+        "rating": 4.69,
+        "images": "assets/Images/images (1).jpeg",
       },
       {
         "id": 2,
-        "title": "Acer",
-        "price": 34000,
-        "discountPercentage": 10,
-        "rating": 3,
+        "title": "Sony WH-XB900N ",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "images":
+            '/Users/r38/StudioProjects/e_com_app/assets/Images/gsmarena_001.jpg',
       },
       {
         "id": 3,
-        "title": "Dell",
-        "price": 30000,
+        "title": "JBL E45B",
+        "price": 549,
         "discountPercentage": 12.96,
-        "rating": 3.5,
+        "rating": 4.69,
+        "images":
+            '/Users/r38/StudioProjects/e_com_app/assets/Images/images.jpeg',
+      },
+      {
+        "id": 4,
+        "title": "Apple AirPods Pro",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "images": 'assets/Images/download (1).jpeg',
+      },
+      {
+        "id": 5,
+        "title": "Bose QuietComfort 35 II",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "images":
+            "/Users/r38/StudioProjects/e_com_app/assets/Images/download.jpeg",
       },
     ],
-  }
+  },
 ];
