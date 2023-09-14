82b828fc39000e3b09aa944f2c731d68ac028d71
amishad7 <118448879+amishad7@users.noreply.github.com>
2023-09-14 15:04:44 +0530

    Update DeatilPage.dart

--- lib/DeatilPage.dart
+++ lib/DeatilPage.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class DeatailPage extends StatefulWidget {
-  const DeatailPage({super.key});
+  DeatailPage({
+    super.key,
+  });
 
   @override
   State<DeatailPage> createState() => _DeatailPageState();
@@ -10,13 +12,153 @@ class DeatailPage extends StatefulWidget {
 class _DeatailPageState extends State<DeatailPage> {
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text(
+        backgroundColor: Colors.white,
+        actions: [
+          const Icon(
+            Icons.shopping_cart,
+            color: Colors.black,
+          ),
+        ],
+        title: const Text(
           "Deatil Page",
           style: TextStyle(color: Colors.black),
         ),
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Container(
+            height: height / 3,
+            width: height / 1,
+            padding: EdgeInsets.all(30),
+            decoration: const BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                    blurStyle: BlurStyle.outer,
+                    color: Colors.grey,
+                    blurRadius: 10,
+                    offset: Offset(0, -4)),
+              ],
+              color: Colors.white,
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(30),
+              ),
+            ),
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      "Sony MDR-ZX110AP",
+                      style: TextStyle(
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(width: 170),
+                    Text(
+                      "\$ 549",
+                      style: TextStyle(
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.redAccent,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "Headphone",
+                      style: TextStyle(
+                        fontSize: 13,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    SizedBox(width: 310),
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow,
+                    ),
+                    Text(
+                      "4.4",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "Brand",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(width: 170),
+                    Text(
+                      "Sony",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "Stock",
+                      style: TextStyle(
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(width: 170),
+                    Text(
+                      "23",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "Description",
+                      style: TextStyle(
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(width: 170),
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
