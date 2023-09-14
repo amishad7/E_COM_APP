4185f0d9467c07a40a64a2baf1b25d190afaf9c7
amishad7 <118448879+amishad7@users.noreply.github.com>
2023-09-14 11:16:57 +0530

    Update Global.dart

--- lib/utils/Global.dart
+++ lib/utils/Global.dart
@@ -95,4 +95,149 @@ List allProducts = [
       },
     ],
   },
+  {
+    'CatogeryName': 'speakers',
+    'data': [
+      {
+        "id": 1,
+        "title": "Sony MDR-ZX110AP",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "images": "assets/Images/images (1).jpeg",
+      },
+      {
+        "id": 2,
+        "title": "Sony WH-XB900N ",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "images":
+            '/Users/r38/StudioProjects/e_com_app/assets/Images/gsmarena_001.jpg',
+      },
+      {
+        "id": 3,
+        "title": "JBL E45B",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "images":
+            '/Users/r38/StudioProjects/e_com_app/assets/Images/images.jpeg',
+      },
+      {
+        "id": 4,
+        "title": "Apple AirPods Pro",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "images": 'assets/Images/download (1).jpeg',
+      },
+      {
+        "id": 5,
+        "title": "Bose QuietComfort 35 II",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "images":
+            "/Users/r38/StudioProjects/e_com_app/assets/Images/download.jpeg",
+      },
+    ],
+  },
+  {
+    'CatogeryName': 'smartphones',
+    'data': [
+      {
+        "id": 1,
+        "title": "Sony MDR-ZX110AP",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "images": "assets/Images/images (1).jpeg",
+      },
+      {
+        "id": 2,
+        "title": "Sony WH-XB900N ",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "images":
+            '/Users/r38/StudioProjects/e_com_app/assets/Images/gsmarena_001.jpg',
+      },
+      {
+        "id": 3,
+        "title": "JBL E45B",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "images":
+            '/Users/r38/StudioProjects/e_com_app/assets/Images/images.jpeg',
+      },
+      {
+        "id": 4,
+        "title": "Apple AirPods Pro",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "images": 'assets/Images/download (1).jpeg',
+      },
+      {
+        "id": 5,
+        "title": "Bose QuietComfort 35 II",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "images":
+            "/Users/r38/StudioProjects/e_com_app/assets/Images/download.jpeg",
+      },
+    ],
+  },
+{
+    'CatogeryName': 'Laptops',
+    'data': [
+      {
+        "id": 1,
+        "title": "Sony MDR-ZX110AP",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "images": "assets/Images/images (1).jpeg",
+      },
+      {
+        "id": 2,
+        "title": "Sony WH-XB900N ",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "images":
+            '/Users/r38/StudioProjects/e_com_app/assets/Images/gsmarena_001.jpg',
+      },
+      {
+        "id": 3,
+        "title": "JBL E45B",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "images":
+            '/Users/r38/StudioProjects/e_com_app/assets/Images/images.jpeg',
+      },
+      {
+        "id": 4,
+        "title": "Apple AirPods Pro",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "images": 'assets/Images/download (1).jpeg',
+      },
+      {
+        "id": 5,
+        "title": "Bose QuietComfort 35 II",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "images":
+            "/Users/r38/StudioProjects/e_com_app/assets/Images/download.jpeg",
+      },
+    ],
+  },
+
 ];
