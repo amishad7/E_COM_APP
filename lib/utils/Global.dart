2c2b48b8f7de65ef15fd38df4e8e267e921b1eb0
amishad7 <118448879+amishad7@users.noreply.github.com>
2023-09-19 11:10:21 +0530

    Update Global.dart

--- lib/utils/Global.dart
+++ lib/utils/Global.dart
@@ -250,5 +250,139 @@ List allProducts = [
         "thumbnail": "https://i.dummyjson.com/data/products/2/1.jpg",
       },
     ],
+  },  {
+    'CatogeryName': 'Tablets',
+    'data': [
+      {
+        "id": 1,
+        "title": "Apple iPad",
+        "price": 990,
+        "discountPercentage": "-",
+        "rating": 5.0,
+          "stock": 98,
+        "brand": "Apple",
+        "thumbnail": "assets/Images/images (1).jpeg",
+        "description":
+            'Known for its premium build quality and iOS operating system, Apples iPad is a popular choice for both productivity and entertainment.',
+      },
+      {
+        "id": 2,
+        "title": "Samsung Galaxy Tab ",
+        "price": 1000,
+        "discountPercentage": 16.9,
+        "rating": 5.0,
+          "stock": 87,
+        "brand": "Samsung",
+        "thumbnail":
+            '/Users/r32/StudioProjects/E_COM_APP/assets/Images/gsmarena_001.jpg',
+        "description":
+            'Samsung offers a range of Android-based tablets under the Galaxy Tab series, known for their versatility and high-resolution displays.',
+      },
+      {
+        "id": 3,
+        "title": "Amazon Fire Tablet",
+        "price": 54,
+        "discountPercentage": 19.6,
+        "rating": 3.67,
+          "stock": 77,
+        "brand": "Amzon",
+        "thumbnail":
+            '/Users/r32/StudioProjects/E_COM_APP/assets/Images/images.jpeg',
+        "description":
+            'Amazons Fire Tablets are affordable options with access to a wide range of apps, e-books, and Amazon services like Kindle and Prime Video.',
+      },
+      {
+        "id": 4,
+        "title": "Microsoft Surface",
+        "price": 300,
+        "discountPercentage": 25.00,
+        "rating": 5.0,
+          "stock": 47,
+        "brand": "Microsoft",
+        "description":
+            'Microsofts Surface tablets are known for their productivity features and the ability to run Windows applications, making them suitable for work and creativity.',
+        "thumbnail": 'assets/Images/download (1).jpeg',
+      },
+      {
+        "id": 5,
+        "title": "Lenovo Tab",
+        "price": 500,
+        "discountPercentage": 36.0,
+        "rating": 4.00,
+          "stock": 47,
+        "brand": "Lenovo",
+        "description":
+            'Lenovo offers a variety of Android-based tablets under the Tab series, known for their sleek design and performance.',
+        "thumbnail":
+            "/Users/r32/StudioProjects/E_COM_APP/assets/Images/download.jpeg",
+      },
+    ],
   },
+  {
+    'CatogeryName': 'Smartwatches',
+    'data': [
+      {
+        "id": 1,
+        "title": "Apple Watch series 5",
+        "price": 50,
+        "discountPercentage": "--",
+        "rating": 5.00,
+          "stock": 97,
+        "brand": "Apple",
+        "thumbnail": "assets/Images/images (1).jpeg",
+        "description":
+            'Known for its premium build quality and iOS operating system, Apples iPad is a popular choice for both productivity and entertainment.',
+      },
+      {
+        "id": 2,
+        "title": "Samsung Galaxy Watch Active2 ",
+        "price": 60,
+        "discountPercentage": "--",
+        "rating": 5.00,
+          "stock": 90,
+        "brand": "Samsung",
+        "images":
+            '/Users/r32/StudioProjects/E_COM_APP/assets/Images/gsmarena_001.jpg',
+        "description":
+            'Samsung offers a range of Android-based tablets under the Galaxy Tab series, known for their versatility and high-resolution displays.',
+      },
+      {
+        "id": 3,
+        "title": "Google pixel watch",
+        "price": 49,
+        "discountPercentage": 15.90,
+        "rating": 4.6,
+          "stock": 87,
+        "brand": "Google",
+        "images":
+            '/Users/r32/StudioProjects/E_COM_APP/assets/Images/images.jpeg',
+        "description":
+            'Amazons Fire Tablets are affordable options with access to a wide range of apps, e-books, and Amazon services like Kindle and Prime Video.',
+      },
+      {
+        "id": 4,
+        "title": "One Plus Watch",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+          "stock": 23,
+        "brand": "MI",
+        "description":
+            'Microsofts Surface tablets are known for their productivity features and the ability to run Windows applications, making them suitable for work and creativity.',
+        "thumbnail": 'assets/Images/download (1).jpeg',
+      },
+      {
+        "id": 5,
+        "title": "Huawei Watch GT3",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "description":
+            'Lenovo offers a variety of Android-based tablets under the Tab series, known for their sleek design and performance.',
+        "thumbnail":
+            "/Users/r32/StudioProjects/E_COM_APP/assets/Images/download.jpeg",
+      },
+    ],
+  },
+  
 ];
